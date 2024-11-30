dc9eac164c4fa7ba3a225cb6f219ce9cab2feea4
KARTHICK <145643933+Karthick-noone@users.noreply.github.com>
2024-11-30 12:39:04 +0530

    Update smsService.js

--- smsService.js
+++ smsService.js
@@ -2,23 +2,11 @@ const axios = require("axios");
 
 // Function to send OTP via SMS
 const sendOtpSms = async (number, otp) => {
-  const smsApiUrl = `http://login.smsgatewayhub.com/api/mt/SendSMS`;
-  const params = {
-    user: "Seasensesoftwares",
-    password: "Stripl@1",
-    senderid: "SEASEN",
-    channel: "Trans",
-    DCS: 0,
-    flashsms: 0,
-    number,
-    text: `Dear ${otp}, Many more happy returns of the day. With regards Sea Sense Group.`,
-    route: 47,
-    DLTTemplateId: "1707161044624969443",
-    PEID: "1701159125640974053",
-  };
+  // Concatenate all parameters directly into the URL
+  const smsApiUrl = `http://login.smsgatewayhub.com/api/mt/SendSMS?user=Seasensesoftwares&password=Stripl@1&senderid=SEASEN&channel=Trans&DCS=0&flashsms=0&number=${number}&text=Dear ${otp}, Many more happy returns of the day. With regards Sea Sense Group.&route=47&DLTTemplateId=1707161044624969443&PEID=1701159125640974053`;
 
   try {
-    const response = await axios.get(smsApiUrl, { params });
+    const response = await axios.get(smsApiUrl);
     console.log(`SMS sent to ${number} successfully. Response:`, response.data);
     return true;
   } catch (error) {
